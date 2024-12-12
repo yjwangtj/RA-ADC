5bce0118c588419cbd7a21418d972bc266ca8912
Yujin Wang <153347241+yjwangtj@users.noreply.github.com>
2024-12-12 20:22:46 +0800

    Update Query_XModal_Attention.py

--- embedding_&_retrieving/Query_XModal_Attention.py
+++ embedding_&_retrieving/Query_XModal_Attention.py
@@ -8,17 +8,16 @@ from langchain_community.document_loaders import PyPDFLoader
 from FlagEmbedding import FlagModel
 
 
-# 加载图像编码模型
 model_image = BlipForConditionalGeneration.from_pretrained("/home/wyj/0-Research_Projects/Retrieval-Augmented Autonomous Driving Corner Case Comprehension with Vision-Language Models/saved_blip")
 processor_image = BlipProcessor.from_pretrained("/home/wyj/blip-image-captioning-base")
 
 
 
-# Load TXT file
+
 with open("/home/wyj/0-Research_Projects/Retrieval-Augmented Autonomous Driving Corner Case Comprehension with Vision-Language Models/datasets/mplug_owl3/sorted_Ours_mplug_owl3_refined_desc.txt", "r") as file:
     text_content = file.readlines()
 
-# Chunking by lines
+
 def split_document_by_lines(text_lines):
     chunks = []
     for line in text_lines:
@@ -31,11 +30,11 @@ docs = split_document_by_lines(text_content)
 with open('Text_Database.pkl', 'rb') as f:
     embeddings = pickle.load(f)
 
-# Loading the database dictionary
+
 with open('Cross_Modal_Database.pkl', 'rb') as f:
     embedding_dict = pickle.load(f)
 
-# 使用新的图像进行查询
+
 new_image = Image.open("/home/wyj/1/base-val-1500/images/000001_1616005007200.jpg")
 new_text = ""
 new_inputs = processor_image(images=new_image, text=new_text, return_tensors="pt")
@@ -49,8 +48,8 @@ def cosine_similarity(vec1, vec2):
         return 0
     return np.dot(vec1, vec2) / denom
 
-# Searching for the most similar embedding with cosine similarity
-alpha = 0 # Weight of XModal embeddings
+
+alpha = 0 
 max_similarity = -1
 retrieved_text = None
 retrieved_image_idx = None
@@ -64,7 +63,7 @@ for idx, img_emb in enumerate(embedding_dict.keys()):
     if similarity > max_similarity:
         max_similarity = similarity
         retrieved_text = embedding_dict[img_emb]
-        retrieved_image_idx = idx + 1  # Keep track of the image index
+        retrieved_image_idx = idx + 1  
 
 print("Max similarity:", max_similarity)
 
@@ -74,7 +73,7 @@ def find_matching_embedding(embeddings, target_embedding):
             return idx
     return -1
 
-# Find the matching text index
+
 matching_idx = find_matching_embedding(embeddings, retrieved_text)
 if matching_idx != -1:
     retrieved_text_plain = docs[matching_idx]
